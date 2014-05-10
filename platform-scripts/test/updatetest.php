bdd4bdbae118a45baee511e250ee3f4faa809b02
Saurabh Sood <saurabhsood91@gmail.com>
2014-05-10 13:13:33 +0530

    added script to update coordinates for particular uuid

---
+++ platform-scripts/test/updatetest.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Update Coordinates</title>
+</head>
+<body>
+
+	<form method="POST" action="updatecoordinates.php">
+
+		<input type="text" name="uuid" /><br />
+		<input type="text" name="lat" /><br />
+		<input type="text" name="lng" /><br />
+		<input type="submit" value="Submit" />
+
+	</form>
+
+</body>
+</html>
---
+++ platform-scripts/updatecoordinates.php
@@ -0,0 +1,18 @@
+<?php 
+include_once('dbconfig.php');
+
+$uuid = $_POST['uuid'];
+$lat = $_POST['lat'];
+$long = $_POST['lng'];
+
+$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die('Failed to Connect to Database');
+$query = sprintf("update users set lat=%f, lng=%f where uuid='%s'", $lat, $long, $uuid);
+echo $query;
+$res = mysqli_query($conn, $query) or die('Failed to execute Query');
+
+if(mysqli_affected_rows($conn) != 1) {
+	//Failed to update uuid
+	echo "-1";
+} 
+
+?>
