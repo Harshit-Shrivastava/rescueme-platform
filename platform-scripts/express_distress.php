80e3955285b8205c839fcaa6b5fb96b38d7ead3f
Saurabh Sood <saurabhsood91@gmail.com>
2014-05-11 05:07:46 +0530

    expres distress

---
+++ platform-scripts/express_distress.php
@@ -0,0 +1,29 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+include_once('dbconfig.php');
+
+$uuid = $_POST['uuid'];
+$lat = $_POST['lat'];
+$long = $_POST['lng'];
+
+
+$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die('Failed to Connect to Database');
+$query = sprintf("insert into distress(uuid, lat, lng) values('%s', %f, %f)", $uuid, $lat, $long);
+//echo $query;
+$res = mysqli_query($conn, $query) or die('Failed to execute Query');
+
+if(mysqli_affected_rows($conn) != 1) {
+	//Failed to insert uuid
+	$json_ob = array("status" => -1);
+//print_r($json_ob);
+
+
+} else {
+	$json_ob = array("status" => 0);
+}
+
+header('Content-Type: application/json');
+//echo $jss;
+echo json_encode($json_ob);
+
+?>
